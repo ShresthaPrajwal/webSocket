c50533742e68ed40a92c09f07f830a980b4487d1
Abhinas <aviregmi2018@gmail.com>
2021-12-15 08:49:42 +0545

    js

--- static/newindex.js
+++ static/newindex.js
@@ -15,14 +15,20 @@ let roomsbutton = document.getElementById('roomdiv');
 let profilename = document.getElementById('Profilename');
 let createroom = document.getElementById('createroom');
 let loginform = document.getElementById('userloginform');
+let signinform = document.getElementById('usersigninform');
+let nextpagelogin = document.getElementById('nextpagelogin');
+let nextpagesignin = document.getElementById('nextpagesignup');
+let loginclick = document.getElementById('loginButton');
+let signinclick = document.getElementById('signinButton');
 let loginusername = document.getElementById('loginUsername');
 let loginpassword = document.getElementById('loginpassword');
-let loginclick = document.getElementById('loginButton');
-let signinclick = document.getElementById('signupButton');
 let warning = document.getElementById('loginmsg');
+let warning2 = document.getElementById('warning2');
 let container = document.getElementById('containers');
-
-
+let emaildiv = document.getElementById('signinemail');
+let usernamediv = document.getElementById('signinusername');
+let passdiv1 = document.getElementById('signin1password');
+let passdiv2 = document.getElementById('signin2password');
 ////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////
 //create members for browserdata
@@ -60,9 +66,11 @@ const validateuser = async (usr, pass) => {
 ///////////////////////////////////////////////////////////
 ///check localstorage for username and password
 if (!localStorage.getItem("browserdata")) {
+
+
     ///////////////////////////////////////////////
     ///////////////////////////////////////////////
-    ///ask for username and password
+    ///ask for username and password for login
     loginclick.addEventListener('click', (e) => {
 
         const username = loginusername.value;
@@ -77,6 +85,43 @@ if (!localStorage.getItem("browserdata")) {
         //check in the database if valid
         validateuser(username, password);
 
+    });
+
+
+    ////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    //get user detalis for signup
+    nextpagesignin.addEventListener('click', (e)=>{
+        signinform.style.display = 'flex';
+        loginform.style.display = 'none';
+
+        /////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////
+        //listen for submit of data
+        signinclick.addEventListener('click', (e)=>{
+            const signinusrname = usernamediv.value;
+            const signinemail = emaildiv.value;
+            const pass1 = passdiv1.value;
+            const pass2 = passdiv2.value;
+
+            console.log(signinusrname, signinemail, pass1);
+            if( true ){
+                ////////////////////////////////////////////////
+                ////////////////////////////////////////////////
+                //send details to the server for further processing
+                socket.emit('newuseradd', {signinusrname, signinemail}, ( response ) => {
+                    console.log(response);
+                })
+
+            }else{
+                warning2.value = "Passwords doesn't match";
+            }
+        });
+    })
+
+    nextpagelogin.addEventListener('click', (e)=>{
+        loginform.style.display = 'flex';
+        signinform.style.display = 'none';
     })
 
 
