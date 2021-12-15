f64713c8e7c3565351b5502dc6c71daa0e3aedbf
Abhinas <aviregmi2018@gmail.com>
2021-12-16 02:01:18 +0545

    auth

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const { Server } = require('socket.io');
 
 
 require("dotenv").config();
-const uri = process.env.URI;
 const PORT = process.env.PORT || 3333;
 
 const app = express();
@@ -15,109 +14,219 @@ const Schema = mongoose.Schema;
 app.use(express.static('static'));
 
 /////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////
-
-//now we will connect to database
-mongoose
-  .connect(process.env.URI, { useNewUrlParser: true })
-  .then(() => console.log('DB Connected'));
-
-mongoose.connection.on('error', (err) => {
-  console.log(`DB connection error: ${err.message}`);
+//connect to the database
+const messagedatabase = mongoose.createConnection(process.env.URI1, { useNewUrlParser: true }, () => {
+    console.log('connected to messagedatabase');
+});
+const userdatabase = mongoose.createConnection(process.env.URI2, { useNewUrlParser: true }, () => {
+    console.log('connected to userdatabase');
 });
 
-    /////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////
-    //schema
 
-    const userSchema = new Schema({
-        name: { type: String },
-        text: { type: String }
-    });
+/////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+//schema
+
+const userSchema = new Schema({
+    name: { type: String },
+    text: { type: String }
+});
+
+//schema for userdetails
+const userdetails = new Schema({
+    name: { type: String },
+    pass: { type: String },
+    email: { type: String },
+    rooms: { type: Array },
+    created: { type: Array },
+})
+///////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+
+io.on('connection', (socket) => {
+
+    ////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
+    //sending rooms details to that user
+    socket.on('validateusr', (data, response) => {
+        let responsedata = {};
+        responsedata.valid = false;
+
+        const schema = userdatabase.model('Userdetails', userdetails);
+        schema.findOne({ name: data.usr }).then(item => {
+
+            if (item != null) {
+
+                if (item.pass == data.pass) {
+                    responsedata.valid = true;
+                    responsedata.usr = data.usr;
+                    responsedata.pass = data.pass;
+                    responsedata.rooms = item.rooms;
+                } else {
+                    responsedata.message = "Invalid Credentials";
+                }
+            } else {
+                responsedata.message = "No such username present!";
+            }
+            response(responsedata);
+
+        })
+    })
+
 
     ///////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    //when signin form is clicked
+    socket.on('newuseradd', (data, response) => {
+        let responsedata = {};
 
+        responsedata.valid = false;
+        const schema = userdatabase.model('Userdetails', userdetails);
 
-    io.on('connection', (socket) => {
+        schema.findOne({ name: data.usr }).then(
+            item => {
+                if (item == null) {
+                    schema.findOne({ email: data.email }).then(
+                        item2 => {
+                            if (item2 == null) {
+                                responsedata.valid = true;
+                                let savethisdata = {
+                                    name: data.usr,
+                                    pass: data.pass,
+                                    email: data.email,
+                                    rooms: [],
+                                    created: []
+                                };
 
+                                const save = new schema(savethisdata);
+                                save.save().then(
+                                    result => {
+                                        response(responsedata);
+                                    }
+                                ).catch(
+                                    err => {
+                                        console.log(err);
+                                    }
+                                )
 
-        ////////////////////////////////////////////////////////////////////
-        ///////////////////////////////////////////////////////////////////
-        //sending rooms details to that user
-        socket.on('validateusr', ( data, response) => {
-            let responsedata = {};
-            responsedata.valid = false;
-            if( data.usr == "abhinas" && data.pass == "abhinas"){
-                responsedata.valid = true;
-            }
-            responsedata.username = data.usr;
-            responsedata.password = data.pass;
-            response(responsedata);
-        })
+                            } else {
+                                responsedata.message = "Email already in use...";
+                                response(responsedata);
+                            }
+                        }
+                    )
+                } else {
+                    responsedata.message = "Username already exists..";
+                    response(responsedata);
 
-
-
-
-
-
-
-
-
-
-
-        ////////////////////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////////////////////
-        //when the user wants to join certain room
-        socket.on('joinRoom', (userinfo) => {
-            socket.join(userinfo.room);
-            ////////////////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////////////
-            ///get messages of that room
-            const schema = mongoose.model( userinfo.room, userSchema);
-            schema.find().limit(20).sort({$natural: -1}).then(
-                items => {
-                    items.forEach( e => {
-                        io.to(socket.id).emit('pastmsg', e.name, e.text);
-                    })
-                    
                 }
-            )
+            }
+        )
+    })
+
+    //when the user wants to join certain room
+    socket.on('joinRoom', (room) => {
+        socket.leaveAll;
+        socket.join(room);
+
+        ///get messages of that room
+        const schema = messagedatabase.model(room, userSchema);
+        schema.find().limit(20).sort({ $natural: -1 }).then(
+            items => {
+                items.forEach(e => {
+                    io.to(socket.id).emit('pastmsg', e.name, e.text);
+                })
+
+            }
+        )
+
+    })
+
+
+    //when the user wants to create new room
+    socket.on('createnewroom', (data, response) => {
+        let responsedata = {};
+        responsedata.valid = false;
+
+        //check if that room already exists
+       // const schema = messagedatabase.model(data.roomname, userSchema);
+        messagedatabase.db.listCollections({'name': data.roomname}).toArray((err, collectionnames)=> {
+            if( err ){
+                console.log(err);
+            }
+            if( collectionnames.length == 0){
+                responsedata.valid = true;
+                messagedatabase.db.createCollection(data.roomname)
+                .then( ()=>{
+                    responsedata.roomname = data.roomname;
+                    const schema = userdatabase.model('Userdetails', userdetails);
+
+                    //update the list of joined room
+                    schema.updateOne({name: data.user}, {
+                        $addToSet: {rooms: [data.roomname]}},
+                        (err, res) =>{
+                            if( err ){
+                                console.log(err);
+                            }
+                        }
+                    )
+
+                    //update the list of created room
+                    schema.updateOne({name: data.user}, {
+                        $addToSet: {created: [data.roomname]}},
+                        (err, res) =>{
+                            if( err ){
+                                console.log(err);
+                            }
+                        }
+                    )
+                    
+                    response(responsedata);
+                }
+                ).catch(
+                    err => {console.log(err);}
+                )
+            }else{
+                responsedata.message = "room already exists";
+                response(responsedata);
+            }
             
         })
+    })
 
-        /////////////////////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////////////////////
-        //when a user sends message in a room
-        socket.on('message', (userinfo) => {
-            io.to(userinfo.room).emit('messageServer', userinfo);
+    ////////////////////////////////////////////////////////////////////
+    //when a user sends message in a room
+    socket.on('message', (data) => {
+        console.log(socket.rooms);
+        io.to(socket.rooms).emit('receivemessage', data);
 
-            //////////////////////////////////////////////////////////////
-            /////////////////////////////////////////////////////////////
-            //send message to database as room as collection name
-            const schema = mongoose.model( userinfo.room, userSchema);
-            const data = { name: userinfo.username, text: userinfo.message };
-            const todatabase = new schema(data);
-            todatabase.save()
-                .then(e => {
-                })
-                .catch(err => console.log(err));
-        })
-
-
-        //////////////////////////////////////////////////////////////////////
-        /////////////////////////////////////////////////////////////////////
-        //when a user wants to send audio
-        socket.on('clientaudio', (audioblob, room) => {
-            socket.broadcast.to(room).emit('serveraudio', audioblob);
-        })
+        /////////////////////////////////////////////////////////////
+        //send message to database as room as collection name
+        const schema = messagedatabase.model(socket.rooms, userSchema);
+        const dataschema = { name: data.sender, text: data.message };
+        const todatabase = new schema(dataschema);
+        todatabase.save()
+            .then(e => {
+            })
+            .catch(err => console.log(err));
+    })
 
 
-    });
+    //////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+    //when a user wants to send audio
+    socket.on('clientaudio', (audioblob, room) => {
+        socket.broadcast.to(room).emit('serveraudio', audioblob);
+    })
 
 
-    ///////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////
-    //finally start the server
-    server.listen(PORT, () => {
-        console.log(`server has started in port ${PORT}`);});
+});
+
+
+///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//finally start the server
+server.listen(PORT, () => {
+    console.log(`server has started in port ${PORT}`);
+});
--- static/newindex.js
+++ static/newindex.js
@@ -22,11 +22,11 @@ let loginclick = document.getElementById('loginButton');
 let signinclick = document.getElementById('signinButton');
 let loginusername = document.getElementById('loginUsername');
 let loginpassword = document.getElementById('loginpassword');
-let warning = document.getElementById('loginmsg');
+let warning = document.getElementById('warning1');
 let warning2 = document.getElementById('warning2');
 let container = document.getElementById('containers');
-let emaildiv = document.getElementById('signinemail');
 let usernamediv = document.getElementById('signinusername');
+let emaildiv = document.getElementById('signinemail');
 let passdiv1 = document.getElementById('signin1password');
 let passdiv2 = document.getElementById('signin2password');
 ////////////////////////////////////////////////////////////
@@ -34,10 +34,8 @@ let passdiv2 = document.getElementById('signin2password');
 //create members for browserdata
 browserdata.username;
 browserdata.password;
-
-
-
-
+browserdata.recentroom;
+browserdata.rooms = [];
 
 
 ////////////////////////////////////////////////////////////////
@@ -46,8 +44,13 @@ const validateuser = async (usr, pass) => {
 
     await socket.emit('validateusr', { usr, pass }, (response) => {
         if (response.valid == true) {
-            browserdata.username = response.username;
-            browserdata.password = response.password;
+            browserdata.username = response.usr;
+            browserdata.password = response.pass;
+
+            //here the rooms order has to be maintained
+            //do this portoin later
+            browserdata.rooms = response.rooms;
+            browserdata.recentroom = browserdata.rooms[0];
             localStorage.setItem("browserdata", JSON.stringify(browserdata));
 
             /////////////////////////////////////////////////////////////////////
@@ -56,16 +59,54 @@ const validateuser = async (usr, pass) => {
             loginform.style.display = 'none';
             container.style.filter = 'none';
 
+
+
+            //write roomname
+            htmlroom.textContent = browserdata.recentroom;
+            //write profile name
+            profilename.innerHTML = browserdata.username;
+            //load the sidebar
+            siderooms();
+
+
         } else {
-            warning.textContent = "We cannot authorize you at the moment.. <br> Enter your username and password.";
+            warning.textContent = response.message;
+            loginform.style.display = 'flex';
+            container.style.filter = 'blur(5px)';
         }
     });
 }
+
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+const newuseradd = async (usr, pass, email) => {
+
+    await socket.emit('newuseradd', { usr, pass, email }, (response) => {
+        console.log(response, usr, pass, email);
+        if (response.valid == true) {
+            ///////////////////////////////////
+            //remove the sign in page
+            signinform.style.display = 'none';
+            loginform.style.display = 'flex';
+        } else {
+            warning2.textContent = response.message;
+        }
+    })
+}
+
 
 ///////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////
 ///check localstorage for username and password
 if (!localStorage.getItem("browserdata")) {
+
+    //////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    //if no credentials found show the loginpage
+    loginform.style.display = 'flex';
+    container.style.filter = 'blur(5px)';
+    signinform.style.display = 'none';
 
 
     ///////////////////////////////////////////////
@@ -87,49 +128,218 @@ if (!localStorage.getItem("browserdata")) {
 
     });
 
-
     ////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////
-    //get user detalis for signup
-    nextpagesignin.addEventListener('click', (e)=>{
+    ///////////////////////////////////////////////////
+    //get signin details
+    signinclick.addEventListener('click', (e) => {
+        let signinusrname = usernamediv.value;
+        let signinemail = emaildiv.value;
+        let pass1 = passdiv1.value;
+        let pass2 = passdiv2.value;
+
+
+        if ((pass1 == pass2) && (signinusrname != " ")) {
+            ////////////////////////////////////////////////
+            ////////////////////////////////////////////////
+            newuseradd(signinusrname, pass1, signinemail);
+
+        } else {
+            warning2.textContent = "Please try again!!";
+        }
+    });
+
+
+    nextpagesignin.addEventListener('click', (e) => {
         signinform.style.display = 'flex';
         loginform.style.display = 'none';
+    });
 
-        /////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////
-        //listen for submit of data
-        signinclick.addEventListener('click', (e)=>{
-            const signinusrname = usernamediv.value;
-            const signinemail = emaildiv.value;
-            const pass1 = passdiv1.value;
-            const pass2 = passdiv2.value;
 
-            console.log(signinusrname, signinemail, pass1);
-            if( true ){
-                ////////////////////////////////////////////////
-                ////////////////////////////////////////////////
-                //send details to the server for further processing
-                socket.emit('newuseradd', {signinusrname, signinemail}, ( response ) => {
-                    console.log(response);
-                })
-
-            }else{
-                warning2.value = "Passwords doesn't match";
-            }
-        });
-    })
-
-    nextpagelogin.addEventListener('click', (e)=>{
+    nextpagelogin.addEventListener('click', (e) => {
         loginform.style.display = 'flex';
         signinform.style.display = 'none';
     })
 
 
 } else {
-    ////////////////////////////////////////////////
-    ////////////////////////////////////////////////
-    //check in the database if valid
     browserdata = JSON.parse(localStorage.getItem("browserdata"));
     validateuser(browserdata.username, browserdata.password);
 }
 
+
+///////////////////////////////////////////////////////////
+//the validation part has been completed
+
+
+//add rooms to the sidebar
+const siderooms = () => {
+
+    for (let i = 0; i < browserdata.rooms.length; i++) {
+        let buttonid = document.createElement('button');
+        buttonid.setAttribute('id', browserdata.rooms[i]);
+        buttonid.setAttribute('type', 'button');
+        buttonid.textContent = browserdata.rooms[i];
+        roomsbutton.appendChild(buttonid);
+    }
+
+}
+
+//connect to a room for messaging
+roomsbutton.addEventListener('click', (e) => {
+    console.log('join a room please');
+    const id = e.target.id;
+    browserdata.recentroom = id;
+    let index = browserdata.rooms.indexOf(id);
+    let item = browserdata.rooms.splice(index, 1);
+    browserdata.rooms.unshift(item[0]);
+    localStorage.setItem("browserdata", JSON.stringify(browserdata));
+    socket.emit("joinroom", id);
+
+    //now clear all the messages and change room name
+    display.innerHTML = "";
+    htmlroom.textContent = id;
+});
+
+
+//load messges for the room
+socket.on('pastmsg', (sender, message) => {
+
+    let item = document.createElement('div');
+    if (sender == browserdata.username) {
+        sender = " ";
+        item.style.marginLeft = "auto";
+        item.style.marginRight = "1.5rem";
+    } else {
+        sender = sender + " : ";
+    }
+
+    item.textContent = sender + message;
+    display.insertBefore(item, display.firstChild);
+    display.scrollTo(0, display.scrollHeight);
+})
+
+//create a new room
+createroom.addEventListener('click', async () => {
+
+    ///for temporary we will use prompt
+    let roomname = prompt("Enter a new room name: ");
+    let user = browserdata.username;
+
+    if (roomname != null) {
+        await socket.emit('createnewroom', { user, roomname }, (response) => {
+            if (response.valid == true) {
+                browserdata.rooms.unshift(response.roomname);
+                localStorage.setItem("browserdata", JSON.stringify(browserdata));
+                roomsbutton.innerHTML = "";
+                siderooms();
+            } else {
+                console.log(response);
+            }
+        })
+
+    }
+});
+
+
+//send messages to a room
+form.addEventListener('submit', (data) => {
+    data.preventDefault();
+
+    if( input.value ){
+        let message = input.value;
+        let sender = browserdata.username;
+        socket.emit('message', {sender, message});
+        input.value = '';
+      }
+    });
+
+
+//listen to messges in a room
+socket.on('receivemessge', (data) => {
+    let item = document.createElement('div');
+
+    if ( data.sender == browserdata.username) {
+      data.sender = "";
+      item.style.marginLeft = "auto";
+      item.style.marginRight = "1.5rem";
+    }else{
+        data.sender = data.sender + " : ";
+    }
+  
+    item.textContent = data.sender + data.message;
+    display.appendChild(item);
+    display.scrollTo(0, display.scrollHeight);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   // Change room
+// socket.on("changeRoom", (room) => {
+//     socket.in(defaultRoom).broadcast.emit("leaveRoom", {text: "--BROADCAST-- User left room"});
+//     socket.leave(defaultRoom);
+//     socket.room = room;
+//     socket.join(room);
+//     socket.emit("roomChangeNotification", {text: "--ROOM-- You changed room"});
+//     socket.in(room).broadcast.emit("OnewUserNotification", {text: "--ROOM_ALL-- New user joined"});
+//   });
